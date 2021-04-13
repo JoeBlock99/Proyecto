961135106e65b496d8a955e42afec61b55f01fbf
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2021-04-12 18:17:38 -0600

    nada

---
+++ table_rec_nd_ball_tracer.py
@@ -0,0 +1,3 @@
+#https://www.pyimagesearch.com/2018/09/19/pip-install-opencv/
+#https://www.pyimagesearch.com/opencv-tutorials-resources-guides/
+#https://www.pyimagesearch.com/2018/07/23/simple-object-tracking-with-opencv/
