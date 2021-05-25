747c2d73066669302b7d002fa6efddc020cc168b
juanferdeleon <dele17822@uvg.edu.gt>
2021-05-25 14:24:38 -0600

    Result Imgs

--- AWS-Rekognition/main.py
+++ AWS-Rekognition/main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     # Run detection
 
     # #Opens the Video file
-    cap= cv2.VideoCapture('./Videos/3-CUSHION-2.mov')
+    cap= cv2.VideoCapture('./Videos/3-CUSHION-4.mov')
     
     # Frame Counter
     i=0
@@ -81,55 +81,55 @@ if __name__ == "__main__":
         if ret == False:
             break
 
-        if 200 <= i <= 205:
-            # Frame Dimesions
-            height, width, _ = frame.shape
-            frameDim = (height, width)
-            
-            print("\nFrame #", i)
-            print("----------------")
-            
-            # Turn frame into bytes
-            source_bytes = frame_into_bytes(frame)
-            
-            # Detect Labels in scene
-            labels_detected = img_detect(source_bytes, access_key_id, secret_access_key, frameDim)
+        # if 200 <= i <= 205:
+        # Frame Dimesions
+        height, width, _ = frame.shape
+        frameDim = (height, width)
+        
+        print("\nFrame #", i)
+        print("----------------")
+        
+        # Turn frame into bytes
+        source_bytes = frame_into_bytes(frame)
+        
+        # Detect Labels in scene
+        labels_detected = img_detect(source_bytes, access_key_id, secret_access_key, frameDim)
 
-            # Analize Scene
+        # Analize Scene
 
-            # Verify if players ball is in contact with any rail
-            players_ball_new_frame, rail_contact_ctr, rails_touched_new_frame = railContact(labels_detected, playersBall, frameDim, players_ball_previous_frame_info)
+        # Verify if players ball is in contact with any rail
+        players_ball_new_frame, rail_contact_ctr, rails_touched_new_frame = railContact(labels_detected, playersBall, frameDim, players_ball_previous_frame_info)
 
-            if players_ball_new_frame:
-                players_ball_previous_frame_info = (players_ball_new_frame, rails_touched_new_frame)
-                if rail_contact_ctr > 0:
-                    railCtr += 1
+        if players_ball_new_frame:
+            players_ball_previous_frame_info = (players_ball_new_frame, rails_touched_new_frame)
+            if rail_contact_ctr > 0:
+                railCtr += 1
+                show_image = True
+
+        # Extract Balls Positions, get if balls are in contact
+        player_ball_current_frame, opponent_ball_current_frame, red_ball_current_frame, touch_between_balls = ballsContact(labels_detected, playersBall, frameDim)
+
+        # If balls are in contact
+        if type(touch_between_balls) is tuple:
+            if touch_between_balls[1]:
+                if (touch_between_balls[0] == "player_red_touch"):
+                    red_ball_touched = True
                     show_image = True
+                if (touch_between_balls[0] == "player_opponent_touch"):
+                    opponent_ball_touched = True
+                    show_image = True
+        
+        if show_image:
+            # Save reference frame
+            cv2.imwrite('./Img/3-CUSHION-'+str(i)+'.jpg',frame)
+            # Display Frame with bounding boxes
+            display_image('./Img/3-CUSHION-'+str(i)+'.jpg', labels_detected)
+            show_image = False
 
-            # Extract Balls Positions, get if balls are in contact
-            player_ball_current_frame, opponent_ball_current_frame, red_ball_current_frame, touch_between_balls = ballsContact(labels_detected, playersBall, frameDim)
-
-            # If balls are in contact
-            if type(touch_between_balls) is tuple:
-                if touch_between_balls[1]:
-                    if (touch_between_balls[0] == "player_red_touch"):
-                        red_ball_touched = True
-                        show_image = True
-                    if (touch_between_balls[0] == "player_opponent_touch"):
-                        opponent_ball_touched = True
-                        show_image = True
-            
-            if show_image:
-                # Save reference frame
-                cv2.imwrite('./Img/3-CUSHION-'+str(i)+'.jpg',frame)
-                # Display Frame with bounding boxes
-                display_image('./Img/3-CUSHION-'+str(i)+'.jpg', labels_detected)
-                show_image = False
-
-            # Print Frame analysis results
-            print("Rail touches", railCtr)
-            print("Contact with red ball: ", red_ball_touched)
-            print("Contact with opponent's ball: ", opponent_ball_touched)
+        # Print Frame analysis results
+        print("Rail touches", railCtr)
+        print("Contact with red ball: ", red_ball_touched)
+        print("Contact with opponent's ball: ", opponent_ball_touched)
         
         # Next Frame
         i+=1
